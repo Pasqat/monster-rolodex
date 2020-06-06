1d3636563ce9e43de57571142465e079f5b19e83
Pasqat <pasquale.matarrese@gmail.com>
2020-06-06 19:17:43 +0200

    Updates

---
+++ precache-manifest.bdc8df823f6e6e6fbf35b089c43943af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fb46455e2a2ffc238b7da6e091ebb6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b82b4aaeb69e61126a4",
+    "url": "/monsters-rolodex/static/css/main.caa3c471.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b82b4aaeb69e61126a4",
+    "url": "/monsters-rolodex/static/js/main.23832bac.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
